344d0e2d1358c82225af4881d1c8c3868bb7bf2a
Yuriy <sheff_ne@yahoo.com>
2014-10-09 19:52:03 -0500

    Initial Commit

---
+++ OutOfThisWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutOfThisWorld
+//
+//  Created by Yuriy Shefer on 10/9/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OutOfThisWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutOfThisWorld
+//
+//  Created by Yuriy Shefer on 10/9/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
